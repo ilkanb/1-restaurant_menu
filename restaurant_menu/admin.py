e1fe6c715b34a30be1ebdfca5acbfd848aa44298
acer <ilkanbayiroglu@gmail.com>
2024-09-29 22:19:27 +0300

    -m

---
+++ restaurant_menu/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Item
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status",)
+    search_fields = ("meal", "description")
+
+admin.site.register(Item, MenuItemAdmin)
---
+++ restaurant_menu/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Item, MEAL_TYPE
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("-date_created")
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        return context
+
+
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
